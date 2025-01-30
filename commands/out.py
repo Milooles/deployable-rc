4a2882c8764880c260052fd53245093a8b0ab38e
Milooles <doodoododo1701@gmail.com>
2025-01-31 09:13:31 +1000

    updated out.py

--- commands/out.py
+++ commands/out.py
@@ -3,4 +3,4 @@ import os
 
 def main():
     sys.argv.pop(0)
-    os.system(f"osascript -e 'display notification \"{" ".join(sys.argv)}\"'")
+    os.system(f"osascript -e 'display notification \"" + " ".join(sys.argv) + "\"'")
