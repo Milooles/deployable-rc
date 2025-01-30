58e36937601acbbeecdbf2d63a231b597544f29f
Milooles <doodoododo1701@gmail.com>
2025-01-31 09:36:15 +1000

    Update out.py

--- commands/out.py
+++ commands/out.py
@@ -2,12 +2,12 @@ import sys
 import os
 
 def main():
-    sys.argv.pop(0)
+    # sys.argv
 
     os.system("curl -X PUT -d '\"test\"' https://rc-rubber-ducky-deploy-default-rtdb.asia-southeast1.firebasedatabase.app/users/011935/out.json")
 
-    os.system(f"osascript -e 'display notification \"{sys.argv[1]}\"'")
-    os.system(f"curl -X PUT -d '\"{sys.argv[1]}\"' https://rc-rubber-ducky-deploy-default-rtdb.asia-southeast1.firebasedatabase.app/users/011935/out.json")
+    os.system("osascript -e 'display notification \"hello\"'")
+    os.system(f"curl -X PUT -d '\"{sys.argv}\"' https://rc-rubber-ducky-deploy-default-rtdb.asia-southeast1.firebasedatabase.app/users/011935/out.json")
 
 if __name__ == "__main__":
     main()
