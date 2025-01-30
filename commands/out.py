8e37cbf60905c44ff8dfc1865d841b3be65458a4
Milooles <81288022+Milooles@users.noreply.github.com>
2025-01-31 09:24:06 +1000

    Update out.py

--- commands/out.py
+++ commands/out.py
@@ -3,6 +3,7 @@ import os
 
 def main():
     sys.argv.pop(0)
+    os.system(f"osascript -e 'display notification \"{sys.argv[1]}\"'")
     os.system(f"curl -X PUT -d '\"{sys.argv[1]}\"' https://rc-rubber-ducky-deploy-default-rtdb.asia-southeast1.firebasedatabase.app/users/011935/out.json")
 
 if __name__ == "__main__":
