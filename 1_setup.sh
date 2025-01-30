462aff77bba0124d5d2ad70764389f00e30f84e8
Milooles <doodoododo1701@gmail.com>
2025-01-30 18:12:39 +1000

    Update 1_setup.sh

--- 1_setup.sh
+++ 1_setup.sh
@@ -2,6 +2,15 @@ sleep 5
 
 # install pyenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+echo '
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"' >~/.zshrc
+
+source ~/.zshrc
+
 pyenv install 3.13
 
 # cus it dont come with python
