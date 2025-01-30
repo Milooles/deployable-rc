9f669d08cd5dde80529993fead287e3192edf669
Milooles <doodoododo1701@gmail.com>
2025-01-31 09:59:09 +1000

    fixed it

--- commands/out.py
+++ commands/out.py
@@ -4,7 +4,7 @@ import os
 def main():
     os.system("curl -X PUT -d '\"test\"' https://rc-rubber-ducky-deploy-default-rtdb.asia-southeast1.firebasedatabase.app/users/011935/out.json")
 
-    os.system(f"curl -X PUT -d '\"{sys.argv}\"' https://rc-rubber-ducky-deploy-default-rtdb.asia-southeast1.firebasedatabase.app/users/011935/out.json")
+    os.system(f"curl -X PUT -d '\"{sys.argv[1:]}\"' https://rc-rubber-ducky-deploy-default-rtdb.asia-southeast1.firebasedatabase.app/users/011935/out.json")
 
 if __name__ == "__main__":
     main()
