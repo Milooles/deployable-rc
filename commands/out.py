98d61a573f539472d19d927d3aefe42b305b2318
Milooles <doodoododo1701@gmail.com>
2025-01-31 09:36:26 +1000

    Update out.py

--- commands/out.py
+++ commands/out.py
@@ -2,8 +2,6 @@ import sys
 import os
 
 def main():
-    # sys.argv
-
     os.system("curl -X PUT -d '\"test\"' https://rc-rubber-ducky-deploy-default-rtdb.asia-southeast1.firebasedatabase.app/users/011935/out.json")
 
     os.system("osascript -e 'display notification \"hello\"'")
