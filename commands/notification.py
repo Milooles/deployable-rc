c2b619d66de7133fb7680b1bb2d8b39b7b04103b
Milooles <doodoododo1701@gmail.com>
2025-01-31 10:25:24 +1000

    Update notification.py

--- commands/notification.py
+++ commands/notification.py
@@ -3,7 +3,7 @@ import os
 
 def main():
     args = sys.argv[1:]
-    if not args[1]: args[1] = "Notification"
+    if not args[1]: args.append("Notification")
 
     os.system(f"osascript -e 'display notification \"{args[0]}\" with title \"{args[1]}\"'")
 
