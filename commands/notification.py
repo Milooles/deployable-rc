23a79ea24fb3579ef1e92bf31a3dad12a394363f
Milooles <doodoododo1701@gmail.com>
2025-01-31 10:14:30 +1000

    Update notification.py

--- commands/notification.py
+++ commands/notification.py
@@ -3,8 +3,9 @@ import os
 
 def main():
     args = sys.argv[1:]
+    if not args[1]: args[1] = "Notification"
 
-    os.system(f"osascript -e 'display notification \"{args[0]}\"{'with title \"{args[1]}\"' if len(args) > 1 else ''}'")
+    os.system(f"osascript -e 'display notification \"{args[0]}\" with title \"{args[1]}\"'")
 
 if __name__ == "__main__":
     main()
